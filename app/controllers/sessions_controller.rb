6558aabd44128415f95bd41ab52e91f09c362bd5
Alex Kyriakou <alexunder193@gmail.com>
2021-09-16 18:12:02 +0300

    create authentication for login and logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,27 @@ class SessionsController < ApplicationController
   end
 
   def create
-    byebug
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+
+      #authenticate once in next line and remain signin for future requests
+      session[:user_id] = user.id
+
+      flash[:notice] = "Logged in successfully"
+      redirect_to user
+    else
+      #render not redirect to use flash[:notice]. Now we use flash.now[:alert]
+      flash.now[:alert] = "There was something wrong with your login details"
+      render 'new'
+    end
+
   end
 
   def destroy
-
+    #to logout make session's user id nil
+    session[:user_id] = nil
+    flash[:notice] = "Logged out"
+    redirect_to root_path
   end
 
 end
