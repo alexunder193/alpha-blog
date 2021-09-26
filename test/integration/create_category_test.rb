3c7441a44bbdad447a5cd993416be6460ccedaf8
Alex Kyriakou <alexunder193@gmail.com>
2021-09-27 01:46:01 +0300

    Add index categories and integration test for listing all categories

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -12,4 +12,28 @@ class CreateCategoryTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_match "Sports", response.body
   end
+
+  test "get new category form and reject invalid category submission" do
+    get "/categories/new"
+    assert_response :success
+    assert_no_difference 'Category.count' do
+      post categories_path, params: { category: { name: " " } }
+    end
+    # looking for word errors which shows up in case of invalid category in error partial
+    assert_match "errors", response.body
+    # looking for div inside error partial
+    assert_select 'div.alert'
+    assert_select 'h4.alert-heading'
+  end
+
+  test "get new category form and reject same category submission" do
+    category = Category.new(name: "Sports")
+    category.save
+    get "/categories/new"
+    assert_response :success
+    assert_no_difference 'Category.count' do
+      post categories_path, params: { category: { name: "Sports" } }
+    end
+    assert_match "Name has already been taken", response.body
+  end
 end
