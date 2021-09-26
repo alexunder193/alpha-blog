8a14fa9a44c40d227a7bc325610a107da105c660
Alex Kyriakou <alexunder193@gmail.com>
2021-09-27 00:07:25 +0300

    Add create form, controller and integration test

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,18 @@
 class CategoriesController < ApplicationController
 
   def new
+    @category = Category.new
+  end
 
+  def create
+    @category = Category.new(category_params)
+    #@category.user = current_user
+    if @category.save
+      flash[:notice] = "Category was successfully created"
+      redirect_to @category
+    else
+      render 'new'
+    end
   end
 
   def index
@@ -9,6 +20,12 @@ class CategoriesController < ApplicationController
   end
 
   def show
+    @category = Category.find(params[:id])
+  end
 
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
